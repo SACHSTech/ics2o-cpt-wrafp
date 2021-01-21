4f38fb6e39f6b3c9729c628be0e973600e24e045
Kevin Huang <kevinhuang23@ycdsbk12.ca>
2021-01-21 10:15:27 -0500

    Learning how to commit and push on vscode

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ A basic pygame template
  
 import pygame
  
+# Function for the character
+# Define a function that will draw a stickman at a certain location
+def draw_stick_figure(screen, x, y):
+    # Head
+    pygame.draw.ellipse(screen, PURPLE, [x, y, 10, 10], 0)
+    pygame.draw.ellipse(screen, VIOLET, [18+x, 10+y, 15, 15], 0)
+    pygame.draw.ellipse(screen, VIOLET, [-14+x, 6+y, 14, 18], 0)
+
 # Define some colors
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 GREEN    = (   0, 255,   0)
 RED      = ( 255,   0,   0)
+VIOLET   = ( 120,  42, 199)
+PURPLE   = (  79,  18, 140)
  
 pygame.init()
   
@@ -23,6 +33,10 @@ done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
+
+pygame.mouse.set_visible(0)
+
+pygame.image.load("character.png")
  
 # -------- Main Program Loop -----------
 while not done:
@@ -32,12 +46,15 @@ while not done:
             done = True # Flag that we are done so we exit this loop
   
     # --- Game logic should go here
- 
+    pos = pygame.mouse.get_pos()
+    x = pos[0]
+    y = pos[1]
     # --- Drawing code should go here
      
     # First, clear the screen to white or whatever background colour. 
     # Don't put other drawing commands above this, or they will be erased with this command.
     screen.fill(WHITE)
+    draw_stick_figure(screen, x, y)
      
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
